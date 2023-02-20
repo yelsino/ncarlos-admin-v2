b25a3f210304267751e32394f95b3d24b8f1e9fb
Restyled.io <commits@restyled.io>
2023-02-20 16:01:28 +0000

    Restyled by prettier

--- src/Utils/formatDate.js
+++ src/Utils/formatDate.js
@@ -4,7 +4,6 @@ import moment from 'moment'
 
 moment.locale('ES')
 // create a function for format date with momen js
-export const formatDate =
-    (date,
-     format =
-         'MMMM Do YYYY, h:mm:ss a') => { return moment(date).format(format)}
+export const formatDate = (date, format = 'MMMM Do YYYY, h:mm:ss a') => {
+  return moment(date).format(format)
+}
